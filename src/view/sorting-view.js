84aab5c0be18b72009096066381412c75e6e84ae
kittiwake <kittiwake@mail.ru>
2024-07-25 19:09:41 +0300

    m5-t2

--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,32 +1,58 @@
 import AbstractView from '../framework/view/abstract-view.js';
+// import {DataSort as SortMode} from '../constants.js';
+
+const SortMode = [
+  {
+    TYPE: 'day',
+    HTML: 'Day',
+    DISABLED: '',
+    CHECKED: 'checked'
+  },
+  {
+    TYPE: 'event',
+    HTML: 'Event',
+    DISABLED: 'disabled',
+    CHECKED: ''
+  },
+  {
+    TYPE: 'time',
+    HTML: 'Time',
+    DISABLED: '',
+    CHECKED: ''
+  },
+  {
+    TYPE: 'price',
+    HTML: 'Price',
+    DISABLED: '',
+    CHECKED: ''
+  },
+  {
+    TYPE: 'offer',
+    HTML: 'Offers',
+    DISABLED: 'disabled',
+    CHECKED: ''
+  },
+];
+
+function createSortItems() {
+  const sortMenuList = [];
+  SortMode.forEach((sortItem) => {
+    sortMenuList.push(
+      `<div class="trip-sort__item  trip-sort__item--${sortItem.TYPE}">
+      <input id="sort-${sortItem.TYPE}" class="trip-sort__input  visually-hidden" type="radio" 
+        name="trip-sort" value="sort-${sortItem.TYPE}" ${sortItem.DISABLED} ${sortItem.CHECKED}>
+      <label class="trip-sort__btn" for="sort-${sortItem.TYPE}" >${sortItem.HTML}</label>
+    </div>`
+    );
+  });
+
+  return (sortMenuList.join(''));
+}
 
 function createSortMenuTemplate() {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked="">
-      <label class="trip-sort__btn" for="sort-day">Day</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--event">
-      <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled="">
-      <label class="trip-sort__btn" for="sort-event">Event</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--time">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-      <label class="trip-sort__btn" for="sort-time">Time</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-      <label class="trip-sort__btn" for="sort-price">Price</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--offer">
-      <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled="">
-      <label class="trip-sort__btn" for="sort-offer">Offers</label>
-    </div>
+${createSortItems()}
   </form>
 `
   );
@@ -34,8 +60,24 @@ function createSortMenuTemplate() {
 
 export default class SortMenuView extends AbstractView {
 
+  #handleSortTypeChange = null;
+
+  constructor(onSortTypeChange) {
+    super();
+    this.#handleSortTypeChange = onSortTypeChange;
+
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  }
+
   get template() {
     return createSortMenuTemplate();
   }
 
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    this.#handleSortTypeChange(evt.target.value.replace('sort-', ''));
+  };
 }
